9e108d5ba82bb6b0e4030160d6304be1fb523858
Lucas Rouckhout <lucas.rouckhout@gmail.com>
2020-02-06 16:52:13 +0100

    Adding Zsh as a dependency and installing .zshrc with no-deps

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@
 #	- rofi
 echo "####### Installing dependencies needed for i3"
 
-apt install fonts-font-awesome nitrogen python3-tk gnome-flashback pulseaudio-utils rofi i3-gaps python3-pip xbacklight dbus volumeicon-alsa numlockx -y
+apt install zsh fonts-font-awesome nitrogen python3-tk gnome-flashback pulseaudio-utils rofi i3-gaps python3-pip xbacklight dbus volumeicon-alsa numlockx -y
 pip3 install --user bumblebee-status
 pip3 install --user psutil i3ipc
 
