78127fd76e4f5bce1d66747983201030ed8d0e0f
Lucas Rouckhout <lucas.rouckhout@gmail.com>
2020-02-06 14:14:57 +0100

    Adding font-awsome to dependencies

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@
 #	- rofi
 echo "####### Installing dependencies needed for i3"
 
-apt install nitrogen python3-tk gnome-flashback pulseaudio-utils rofi i3-gaps python3-pip xbacklight dbus volumeicon-alsa -y
+apt install fonts-font-awesome nitrogen python3-tk gnome-flashback pulseaudio-utils rofi i3-gaps python3-pip xbacklight dbus volumeicon-alsa -y
 pip3 install --user bumblebee-status
 pip3 install --user psutil i3ipc
 
