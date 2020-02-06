0648d4bdb2db34bcf06cb41d6fff69f059de2655
Lucas Rouckhout <lucas.rouckhout@gmail.com>
2020-02-06 13:41:02 +0100

    bumblebeestatus

--- install.sh
+++ install.sh
@@ -4,7 +4,8 @@
 #	- rofi
 echo "####### Installing dependencies needed for i3"
 
-apt install nitrogen gnome-flashback rofi i3-gaps -y
+apt install nitrogen gnome-flashback rofi i3-gaps python3-pip -y
+pip3 install --user bumblebee-status
 
 echo "####### You will also need neovim..."
 
