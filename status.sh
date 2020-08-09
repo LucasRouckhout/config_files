50bf61c15ab5587de981142626d8f9edb0fe5187
Lucas Rouckhout <lucas.rouckhout@gmail.com>
2020-08-09 17:02:47 +0200

    Looping of status is done in xsessions

--- status.sh
+++ status.sh
@@ -1,12 +1,4 @@
 #!/usr/bin/env bash
 
 # Set the X root window which functions as a DWM statusbar.
-
-RED='\033[0;31m'
-NC='\033[0m' # No Color
-
-while true; 
-do
-	xsetroot -name "[$(hostname -I | awk '{print $1}')]  [$(date +%H:%M:%S)]"
-	sleep 1s
-done;
+xsetroot -name "[$(hostname -I | awk '{print $1}')]  [$(date +%H:%M:%S)]"
