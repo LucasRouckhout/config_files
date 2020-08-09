6553ac30610d838d52606ebe72c535ce99dc44b0
Lucas Rouckhout <lucas.rouckhout@gmail.com>
2020-08-09 10:52:32 +0200

    Beginning of status bar script

--- status.sh
+++ status.sh
@@ -2,8 +2,11 @@
 
 # Set the X root window which functions as a DWM statusbar.
 
+RED='\033[0;31m'
+NC='\033[0m' # No Color
+
 while true; 
 do
-	xsetroot -name "$(date)"
+	xsetroot -name "[$(hostname -I | awk '{print $1}')]  [$(date +%H:%M:%S)]"
 	sleep 1s
 done;
