346cd278395264871c31c2c8876b94d9df97880b
Lucas Rouckhout <lucas.rouckhout@gmail.com>
2020-08-13 21:13:07 +0200

    changing status to add battery

--- status.sh
+++ status.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 # Set the X root window which functions as a DWM statusbar.
-xsetroot -name "[$(hostname -I | awk '{print $1}')]  [$(date)]"
+xsetroot -name "[Battery: $(cat /sys/class/power_supply/BAT0/capacity)%]  [$(hostname -I | awk '{print $1}')]  [$(date)]"
