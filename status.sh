7b99355121ba80a2cd16aa96d772612923029e9e
Lucas Rouckhout <lucas.rouckhout@robovision.ai>
2020-11-02 09:55:39 +0100

    Update ip command in status to use ip command

--- status.sh
+++ status.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 # Set the X root window which functions as a DWM statusbar.
-xsetroot -name "[Battery: $(cat /sys/class/power_supply/BAT0/capacity)%]  [$(hostname -I | awk '{print $1}')]  [$(date)]"
+xsetroot -name "[Battery: $(cat /sys/class/power_supply/BAT0/capacity)%]  [$(ip route get 1.1.1.1 | grep -oP 'src \K\S+')]  [$(date)]"
