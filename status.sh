a04bf130221175e5b123cc0b6ce0231b99c00e66
Lucas Rouckhout <lucas.rouckhout@gmail.com>
2021-05-20 14:00:09 +0200

    Adding while in status

--- status.sh
+++ status.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
 
 # Set the X root window which functions as a DWM statusbar.
-xsetroot -name "[$(ip route get 1.1.1.1 | grep -oP 'src \K\S+')]  [$(date)]"
+while true; do
+    xsetroot -name "[$(ip route get 1.1.1.1 | grep -oP 'src \K\S+')]  [$(date)]"
+    sleep 1
+done
