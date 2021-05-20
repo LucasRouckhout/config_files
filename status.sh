d03b18cbcf2782be660d841bcde5669352c5d24d
Lucas Rouckhout <lucas.rouckhout@gmail.com>
2021-05-20 13:41:39 +0200

    Remove battery

--- status.sh
+++ status.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 # Set the X root window which functions as a DWM statusbar.
-xsetroot -name "[Battery: $(cat /sys/class/power_supply/BAT0/capacity)%]  [$(ip route get 1.1.1.1 | grep -oP 'src \K\S+')]  [$(date)]"
+xsetroot -name "[$(ip route get 1.1.1.1 | grep -oP 'src \K\S+')]  [$(date)]"
