03966b024857f2d758186fe887c4ea5804661fe4
Lucas Rouckhout <lucas.rouckhout@gmail.com>
2021-05-22 13:54:39 +0200

    Pimped up status.sh and alacritty

--- status.sh
+++ status.sh
@@ -1,7 +1,20 @@
 #!/usr/bin/env bash
 
+# Put this file in /etc/profile.d/ to run it on startup
+# Author: Lucas Rouckhout
+
+# Echo out the IP address of this host
+function ipFormat() {
+    echo $(ip route get 1.1.1.1 | grep -oP 'src \K\S+')
+}
+
+# Return formatted date and time
+function timeFormat() {
+    echo $(date +"(%a %b %d) %H:%M %p")
+}
+
 # Set the X root window which functions as a DWM statusbar.
 while true; do
-    xsetroot -name "[$(ip route get 1.1.1.1 | grep -oP 'src \K\S+')]  [$(date)]"
+    xsetroot -name "IP: $(ipFormat)  $(timeFormat)"
     sleep 1
-done
+done &
